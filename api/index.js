8bde3725c72dfe6a78f1e62b0c5d72a4c232796c
freudinsky <timo.fuchs94@icloud.com>
2023-09-21 23:34:49 +0200

    built routes

--- api/index.js
+++ api/index.js
@@ -1,14 +1,48 @@
-import express from "express"
-import { Pool } from "pg"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import pg from "pg";
 
-const pool = new Pool(
-    connectionString = `${process.env.POSTGRES_URL}?sslmode=require`,
-)
+dotenv.config();
 
-const app = express()
+const { Pool } = pg;
+
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+const app = express();
+
+app.get("/search", (req, res) => {
+	const { query } = req.query;
+	pool
+		.query(
+			`SELECT * FROM posts 
+      WHERE "postTitle" ILIKE $1 OR "postText" ILIKE $1;`,
+			[`%${query}%`]
+		)
+		.then((data) => res.json(data.rows))
+		.catch((e) => {
+			console.log("Error:", e);
+			res.sendStatus(500);
+		});
+});
+
+app.get("/post/:id", (req, res) => {
+	const { id } = req.params;
+	pool
+		.query(`SELECT * FROM posts WHERE "postID"=$1`, [id])
+		.then((data) => res.json(data.rows))
+		.catch((e) => res.sendStatus(404));
+});
+
+
+
+app.get("/", (req, res) =>
+	pool
+		.query("SELECT * FROM posts")
+		.then((data) => res.json(data.rows))
+		.catch((e) => res.sendStatus(500))
+);
+
 const server = app.listen(8585, () => {
-    console.log("Server running on Port 8585.")
-})
-
-app.get("/api", (req,res))
+	console.log("Server running on Port 8585.");
+});
