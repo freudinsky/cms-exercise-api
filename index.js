b93e107e052c10b7e3fd3e28d06c07f480adc66d
freudinsky <timo.fuchs94@icloud.com>
2023-09-22 00:23:40 +0200

    ..

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-	connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+	connectionString:
+		"postgres://default:YLKk26jWtumA@ep-dry-hall-77963246-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
 });
 const app = express();
 
