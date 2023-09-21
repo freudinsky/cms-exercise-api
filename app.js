b0e7eacfa54daf36224db2687096d580efc3bddd
freudinsky <timo.fuchs94@icloud.com>
2023-09-21 21:42:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { Pool } from "pg"
+import dotenv from "dotenv"
+
+const pool = new Pool(
+    connectionString = `${process.env.POSTGRES_URL}?sslmode=require`,
+)
+
+const app = express()
+const server = app.listen(8585, () => {
+    console.log("Server running on Port 8585.")
+})
+
+app.get("/", (req,res))
